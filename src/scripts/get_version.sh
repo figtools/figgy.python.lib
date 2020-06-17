01b6b2f88fc45997bed65418f914f20cfdfa6760
Jordan Mance <mancej@snagajob.com>
2020-06-16 23:29:32 -0400

    Fixing build, improving version parsing

---
+++ src/scripts/get_version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+cat "$SCRIPTPATH/../setup.py"| grep -E "VERSION\s+=\s+" | sed -E "s#.*([0-9]+.[0-9]+.[0-9]+[a-zA-Z]?).*#\1#g"
